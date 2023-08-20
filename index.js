944c949ae489c26505d1b7e94e12f44e48c78bd4
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-20 23:12:55 +0530

    commit

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Server is running fine!!");
+});
+
 /* ROUTES */
 app.use("/openai", openAiRoutes);
 app.use("/auth", authRoutes);
